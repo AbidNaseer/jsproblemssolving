a43d2e1a9b565ab3183a5ec4ddcf133bead8dd96
Abid Naseer <abidnaseer454@gmail.com>
2022-03-15 19:32:51 +0500

    task done

---
+++ assignment.js
@@ -0,0 +1,71 @@
+// var a = prompt(parseInt("Enter your value here",""));
+// var b = prompt(parseInt("Enter the second value here",""));
+// if (a > b) {
+//     document.write("A is greater than B ")
+// }
+// else{
+//     document.write("B is greater than A ")
+// }
+// console.log(a);
+// console.log(b);
+// this is the first solution
+
+
+
+// var x = parseInt(prompt("Enter the value here",""));
+// var y = parseInt(prompt("Enter the value here",""));
+// var z = parseInt(prompt("Enter the value here",""));
+// var v = check(x,y,z);
+// console.log(`${x} , ${y} ${z}:  largest = ${v}` );
+// // Second Problem
+// problem num 2 
+// if (a>=b && a >= c) {
+//     var res = a;
+// } else if (b>=a && b >= c) {
+//     var res = b;
+// } else {
+//     var res = c;
+// problem number 3
+// if (a >=0) {
+//     var res = ("a is the positive");
+// } else {
+//     var res = ("a is negitive")
+// }
+// problem no 4
+// if (a >4 && a < 6) {
+//     var res = ("a is dividable ");
+//   } else if (a > 10 && a < 12) {
+//     var res = ("a is dividable");
+//   } else {
+//     var res = ("a is not dividable");
+//   }
+
+// this is the problem number 5
+
+// if (a%2==0 ) {
+//   var res = ("A is even")
+// } else {
+//    var res = ("A is odd")
+// }
+// this is the solution of 6th problem
+// (a%4==0)
+
+
+
+function Check() {
+    var a = document.getElementById("first").value;
+    // var b = document.getElementById("second").value;
+    // var c = document.getElementById("third").value;
+    if () {
+       var res =("it is a leap year Congratulations")
+    } else {
+       var res = ("Still Struggling")
+    }
+      document.getElementById("five").value = res;
+}
+
+
+
+
+
+
